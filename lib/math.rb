e17bf04ab06062273e8d7ed2894f91e09dd07a7d
Kimberly Bone <k_bone@uncg.edu>
2019-06-12 16:33:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,19 +4,21 @@ end
 addition (5, 4)
 
 def subtraction(num1, num2)
-  num2 - num1
+  num1 - num2
 end
 subtraction (10, 5)
 
 def division(num1, num2)
-  num2 / num1
+  num1 / num2
 
 end
+division (50, 2)
 
 def multiplication(num1, num2)
   num1 * num2
 
 end
+multiplication (4, 30)
 
 def modulo(num1, num2)
 
