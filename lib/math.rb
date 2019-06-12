233a752ae17f3a454695c9d23b6a1c317b58788b
Kimberly Bone <k_bone@uncg.edu>
2019-06-12 16:32:04 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,19 @@
 def addition(num1, num2)
+  num1 + num2
 
 end
 
 def subtraction(num1, num2)
-
+  num2 - num1
 end
 
 def division(num1, num2)
+  num2 / num1
 
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 
 end
 
@@ -19,5 +22,6 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
+  math.square_root(num)
 
 end
